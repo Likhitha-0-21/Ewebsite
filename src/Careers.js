e0b3185597c43e213cf747ad27d1e2690d6a37d7
likhitha kosiriddy <114893679+Likhitha-0-21@users.noreply.github.com>
2025-04-05 10:58:31 +0530

    Update Careers.js
    made minor info changes on careers page

--- src/Careers.js
+++ src/Careers.js
@@ -6,7 +6,7 @@ function Careers(){
       <div className="row">
         <div>
           <strong>Join Our Team</strong>
-          <p>Are you passionate about fashion and e-commerce? [Your Website Name] is always looking for talented, driven individuals to join our growing team. We believe in fostering creativity, innovation, and collaboration in everything we do. If you’re looking for a dynamic and exciting work environment, we’d love to hear from you!</p>
+          <p>Are you passionate about fashion and e-commerce? Shein is always looking for talented, driven individuals to join our growing team. We believe in fostering creativity, innovation, and collaboration in everything we do. If you’re looking for a dynamic and exciting work environment, we’d love to hear from you!</p>
         </div>
         <br></br>
         <div className="cc">
@@ -14,7 +14,7 @@ function Careers(){
         </div>
         <div className="cc2">
           <strong>No Worries💪 we've got your back</strong><br></br>
-          <em>If you think you have any new ideas,Send your resume includes your innovation and a cover letter to careers@shein.com and let us know why you’re the perfect fit for our team. We can’t wait to meet you!</em>
+          <em>If you think you have any new ideas,Send your resume includes your innovation and a cover letter to careershere@shein.com and let us know why you’re the perfect fit for our team. We can’t wait to meet you!</em>
         </div>
       </div>
         
